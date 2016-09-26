e5d01d3f0d9f99abecf87d6c39c0a0064b5f1925
Wilson <Wilson@Defrosted_Tuna>
2016-09-26 10:25:35 -0400

    bugg fix?

---
+++ src/gitdemo/cryClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author Wilson
+ */
+public class cryClass {
+    public static void cry(){
+        System.out.println("cry Q__Q");
+    }
+}
